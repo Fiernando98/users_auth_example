2153fcbcc75a9db151547822d9ec25e78378459f
Fernando Lerma <fiernando_lerma@hotmail.com>
2021-09-28 16:12:46 -0600

    auth login

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,5 +1,7 @@
 from drf_query_filter import fields
+from knox.views import LoginView
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
 
 from .models import User
 from .serializer import UserSerializer
@@ -13,3 +15,12 @@ class UserViewSet(viewsets.ModelViewSet):
     query_params = [
         fields.Field('username', 'username__icontains')
     ]
+
+
+# Create your views here.
+class LoginCustomView(LoginView):
+    """
+    This only overrides the original View for using the Basic Authentication instead of the
+    default one
+    """
+    authentication_classes = (BasicAuthentication,)
--- django_files/urls.py
+++ django_files/urls.py
@@ -1,9 +1,11 @@
+from apps.user.views import LoginCustomView
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/login/', LoginCustomView.as_view()),
     path('users/', include('apps.user.urls')),
     path('profiles/', include('apps.custom_profile.urls'))
 ]
